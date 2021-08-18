386426be9cf1632b47524df5f5126e0b339eeb28
carolinejeunon <89053259+carolinejeunon@users.noreply.github.com>
2021-08-18 20:40:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// Atividade 1 
+
+console.log ("Olá, mundo!")
+
+// Atividade 2 
+
+console.log ("Caroline Jeunon Gomes da Cunha\n","Rua Batista de Figueiredo, 107\n","30380720")
+
+// Atividade 3 
+
+let nome = prompt ("nome")
+console.log ("Olá,", nome)
+
+// Atividade 4
+
+let var1 = prompt ("Digite um número: ");
+console.log ("Primeiro valor = ", var1);
+let var2 = prompt ("Digite outro número: ");
+console.log ("Segundo valor = ", var2)
+console.log ("Soma =", parseInt (var1) + parseInt (var2))
+
+// Atividade 5
+
+let n1 = parseInt (prompt ("Primeira nota =",))
+let n2 = parseInt (prompt ("Segunda nota =",))
+let n3 = parseInt (prompt ("Terceira nota =",))
+let media = prompt ((n1 + n2 + n3)/3)
+
+// Atividade 6
+
+let v1 = parseInt (prompt ("Primeiro valor ="))
+let v2 = parseInt (prompt ("Segundo valor ="))
+let resultado = prompt (v1 >= v2)
+
+
+
+
+
